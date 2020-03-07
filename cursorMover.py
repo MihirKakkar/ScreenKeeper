6b2f1566a60da25d14b2a65895f259b6c278130c
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-03-07 00:00:28 -0500

    remove excess

--- cursorMover.py
+++ cursorMover.py
@@ -10,21 +10,13 @@ def cursor_mover():
     start = t.time()
     mouse = Controller()
     i = 0
-    mouse.move(50, 50)
 
-    while i % 2 == 0:
+    while True:
+
         movedirOne = rand.randint(1, 100)
         movedirTwo = rand.randint(1, 100)
         mouse.move(movedirOne, movedirTwo)
         t.sleep(2.0 - ((t.time() - start) % 2))
         i += 1
-
-    while i % 2 == 1:
-        movedirThree = rand.randint(1, 100)
-        movedirFour = rand.randint(1, 100)
-        mouse.move(movedirThree, movedirFour)
-        t.sleep(2.0 - ((t.time() - start) % 2))
-        i += 1
-
 
 cursor_mover()

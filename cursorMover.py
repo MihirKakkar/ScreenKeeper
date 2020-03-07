aa39c52d2e42263b4ab7b395fb982873bb613ec7
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-03-07 00:16:53 -0500

    added keystroke functionality

--- cursorMover.py
+++ cursorMover.py
@@ -1,19 +1,22 @@
 
 import time as t
 import random as rand
-from pynput.mouse import Controller
+from pynput.mouse import Controller as mouseController
+from pynput.keyboard import Key, Controller
 import numpy
 
 #This function will move the cursor every couple of seconds
 
 def cursor_mover():
     start = t.time()
-    mouse = Controller()
+    mouse = mouseController()
+    keyboard = Controller()
 
     while True:
         movedirOne = rand.randint(-100, 100)
         movedirTwo = rand.randint(-100, 100)
         mouse.move(movedirOne, movedirTwo)
-        t.sleep(2.0 - ((t.time() - start) % 2))
+        t.sleep(7.0 - ((t.time() - start) % 7))
+        keyboard.press(Key.space)
 
 cursor_mover()
